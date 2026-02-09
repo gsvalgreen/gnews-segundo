a2c70c334f0925c32bf34f5a4efb5ff3e8bb6c84
Gustavo Santos Valverde <31700432+gsvalgreen@users.noreply.github.com>
2026-02-09 20:22:50 -0300

    Fix duplicate import and address code smells
    Removed duplicate import statement and added comments regarding code smells.

--- src/main/java/com/gnews/fake/FakeGNewsApplication.java
+++ src/main/java/com/gnews/fake/FakeGNewsApplication.java
@@ -3,10 +3,13 @@ package com.gnews.fake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
+import java.util.List;
 
 @SpringBootApplication
 public class FakeGNewsApplication {
 	// ToDo This is a code smell!!
+	// Another code smell
+	// Another code smell
 	// Another code smell
 	public static void main(String[] args) {
 		SpringApplication.run(FakeGNewsApplication.class, args);
